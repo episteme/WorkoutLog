ed6ae2606b806f696f964f01a942ca4ef6ed593d
Marcus Peacock <marcus@ruination.org.uk>
2015-07-09 14:48:36 +0100

    Deleted Fragment Listeners

--- app/src/main/java/com/marcuspeacock/MyFirstApp/workoutlog2/app/MyDialogListener.java
+++
@@ -1,7 +0,0 @@
-package com.marcuspeacock.MyFirstApp.workoutlog2.app;
-
-import java.util.HashMap;
-
-public interface MyDialogListener {
-    public void onReturnValue(HashMap<String, String> values);
-}
